2c47a1e90020fed2c6097643237392df9ef6b9e5
sebastianmontdev <sebastianmont-1199@hotmail.com>
2026-01-08 21:20:23 -0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+//alert('hola desde app.js');
+//
+//console.log('hola mundo');
+let a = 10,
+    b = 20,
+    c = 'hola ',
+    d = 'spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+c = 'hola de nuevo';
+
+//console.log('%c mis variables', 'color:blue; font-weight: bold');
+//console.log({ a });
+//console.log({ b });
+//console.log({ c });
+//console.table({a, b, c, x})
+
+//var outerWidth = 1000000;
