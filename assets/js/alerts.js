f7b7254efc96e08f719b20160125d95b70686ee7
sebastianmontdev <sebastianmont-1199@hotmail.com>
2026-01-09 17:39:10 -0500

    Add files via upload

---
+++ assets/js/alerts.js
@@ -0,0 +1,6 @@
+alert('hola mundo')
+
+let nombre = prompt('¿cual es tu nombre?', 'sin nombre');
+console.log(nombre)
+const  selecion =  confirm('¿esta seguro de borrar esto?');
+//console.log(global);
---
+++ assets/js/primitivos.js
@@ -0,0 +1,5 @@
+let nombre = 'peter parker';
+console.log( nombre );
+
+nombre = 'ben parker';
+console.log(nombre);
