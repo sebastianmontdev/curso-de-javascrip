cad955d2a6421c3ea295108cc7d7340d2a80cf8d
sebastianmontdev <sebastianmont-1199@hotmail.com>
2026-01-10 23:03:58 -0500

    Add files via upload

--- assets/js/primitivos.js
+++ assets/js/primitivos.js
@@ -3,3 +3,28 @@ console.log( nombre );
 
 nombre = 'ben parker';
 console.log(nombre);
+
+nombre = "tia may";
+nombre = 'tia may';
+
+console.log(typeof nombre);
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+let edad = 33;
+console.log(typeof edad);
+
+edad = 33.0001
+console.log(typeof edad);
+
+let superPoderDeSpiderman;
+console.log(typeof superPoderDeSpiderman);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol = Symbol('a');
+let symbol1 = Symbol('a');
+console.log(typeof symbol);
+console.log(symbol === symbol1);
