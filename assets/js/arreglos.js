f8a9ba4b5df307e0082a53bc21f14a13e29e7d3a
sebastianmontdev <sebastianmont-1199@hotmail.com>
2026-01-14 23:31:33 -0500

    Add files via upload

---
+++ assets/js/arreglos.js
@@ -0,0 +1,9 @@
+//const arr = new Array(10);
+//const arr = [];
+//console.log(arr);
+
+let videoJuegos = ['mario3','megaman','chrno trigger'];
+console.log({ videoJuegos  });
+console.log( videoJuegos[0]  );
+
+let arregloDeCosas = [];
---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,22 @@
+let enum = 123;
+
+//const
+
+//if
+
+//try
+
+//while
+
+//for()
+
+//enum
+
+let objeto$ = 123;
+let precio99_99 = 123;
+
+let jugadorConcpcuntajeMasAlto = 'fernando';
+
+class juegoAnio{
+
+}
